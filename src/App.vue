d2b91531c02a0b6e9f33524e8e0fb9759d31f2df
Karim <jkarim.mp@gmail.com>
2022-07-30 04:18:34 -0500

    Se agregó nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,165 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-card id="nav">
+      <v-app-bar flex color="white" elevate-on-scroll app height="80rem" >
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+        <v-app-bar-nav-icon
+          class="hidden-md-and-up"
+          @click="drawer = true"
+        ></v-app-bar-nav-icon>
 
+
+        <v-app-bar-title
+          ><img alt="Addition logo" src="./assets/logo.png"
+        /></v-app-bar-title>
+        <v-spacer></v-spacer>
+      
+          <div class="nav-links hidden-sm-and-down">
+            <v-btn
+            
+              v-for="item in menu"
+              :key="item.key"
+              :to="item.link"
+
+              plain
+              tile
+              >{{ item.title }}
+              
+            </v-btn>
+            <v-btn class="rounded-link" color="#ea5076" dark>
+              Contáctanos <v-icon right dark small> mdi-arrow-right </v-icon>
+            </v-btn>
+          </div>
+<v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list nav dense>
+          <v-list-item-group v-model="group" active-class=" text--accent-4">
+            <v-list-item v-for="(item, i) in menu" :key="i">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+
+      </v-app-bar>
+
+      
+
+      <!--  <v-toolbar id="nav" color="white" fixed elevation="0">
+
+      <v-toolbar-title
+        ><img alt="Addition logo" src="./assets/logo.png"
+      /></v-toolbar-title>
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <div class="nav-links">
+          <v-btn
+            v-for="item in menu"
+            :key="item.key"
+            :to="item.link"
+            color="black"
+            plain
+            >{{ item.title }}
+            <v-icon right dark small>{{ item.icon }} </v-icon>
+          </v-btn>
+          <v-btn class="rounded-link" color="#ea5076" dark>
+            Contáctanos <v-icon right dark small> mdi-arrow-right </v-icon>
+          </v-btn>
+        </div>
+      </v-toolbar-items> -->
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+      <!-- <v-menu class="hidden-md-and-up">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn light icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, i) in menu" :key="i">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item> -->
+
+      <!-- <v-list-tile v-for="item in menu" :key="item.key">
+            <v-list-tile-content>
+              <v-list-title>{{ item.title }}</v-list-title>
+            </v-list-tile-content>
+          </v-list-tile> -->
+      <!-- </v-list>
+      </v-menu> -->
+      <!-- </v-toolbar> -->
+      <div >
+        <router-view />
+      </div>
+
+      <v-footer height="auto" color="indigo" dark>
+        <v-layout justify-center row wrap>
+          <v-flex color="indigo" dark py-3 text-xs-center white--text xs12>
+            &copy;2018 — <strong>Vuetify</strong>
+          </v-flex>
+        </v-layout>
+      </v-footer>
+    </v-card>
   </v-app>
 </template>
 
+<style lang="scss">
+#app {
+  font-family: Commissioner, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  font-family: Montserrat;
+  align-items: center;
+
+ 
+}
+
+
+
+.nav-links {
+
+
+  a {
+    
+    :hover {
+      color: #ea5076;
+    }
+  }
+}
+
+.rounded-link {
+  border-radius: 0.75rem;
+}
+</style>
+
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      menu: [
+        { key: "inicio", icon: "", title: "Inicio", link: "/" },
+        { key: "nosotros", icon: "", title: "Nosotros", link: "/Nosotros" },
+        {
+          key: "servicios",
+          icon: "mdi-menu-down-outline",
+          title: "Servicios",
+          link: "/Servicios",
+        },
+        { key: "blog", icon: "", title: "Blog", link: "/Blog " },
+      ],
+      drawer: false,
+      group: null,
+    };
   },
 
-  data: () => ({
-    //
-  }),
+  methods: {
+    menuItems() {
+      return this.menu;
+    },
+  },
 };
 </script>
--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,14 +1,87 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-    </v-row>
-  </v-container>
+    <v-card>
+        
+    </v-card>
 </template>
+
+<style lang="scss">
+
+.fondo{
+    height: 100vh;
+    padding: 50%;
+
+    background-image: url("../assets/portada/imagen-portada.png");
+
+
+  /* Set a specific height */
+  min-height: 500px;
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+    
+
+   
+}
+</style>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    ecosystem: [
+      {
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader",
+      },
+      {
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify",
+      },
+      {
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify",
+      },
+    ],
+    importantLinks: [
+      {
+        text: "Documentation",
+        href: "https://vuetifyjs.com",
+      },
+      {
+        text: "Chat",
+        href: "https://community.vuetifyjs.com",
+      },
+      {
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify",
+      },
+      {
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs",
+      },
+      {
+        text: "Articles",
+        href: "https://medium.com/vuetify",
+      },
+    ],
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer",
+      },
+      {
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
+      },
+    ],
+  }),
+};
+</script>
