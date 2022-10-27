3edf6f365c0d9d37a5573ba7bd6f6288994de62b
KarMa <jkarim.mp@gmail.com>
2022-10-27 17:36:44 -0500

    Se agregó metodo para enviar correo desde formulario

--- src/views/Contactanos.vue
+++ src/views/Contactanos.vue
@@ -1,10 +1,10 @@
-<template>
+<!-- <template>
   <v-container fluid class="pa-0">
     <p class="text-title texto-rosa">Contáctanos</p>
     <p class="mt-n8">
       Estamos a su disposición para resolver cualquier tipo de duda
     </p>
-    <v-form v-model="valid">
+    <form>
       <v-container>
         <v-row class="px-16">
           <v-col cols="12" md="6">
@@ -12,7 +12,7 @@
               outlined
               v-model="firstname"
               :rules="nameRules"
-              label="Nombre"
+              label="nombre"
               required
             ></v-text-field>
           </v-col>
@@ -22,7 +22,7 @@
               outlined
               v-model="email"
               :rules="emailRules"
-              label="E-mail"
+              label="email"
               required
             ></v-text-field>
           </v-col>
@@ -30,7 +30,7 @@
           <v-col cols="12" md="12">
             <v-textarea
               outlined
-              label="Mensaje"
+              label="mensaje"
               v-model="lastname"
               :counter="150"
               :rules="msgRules"
@@ -39,7 +39,8 @@
           </v-col>
         </v-row>
       </v-container>
-    </v-form>
+      <input type="submit" value="Send">
+    </form>
     <div class="pa-5">
       <v-btn color="#ea5076" style="text-transform: none" dark>Enviar</v-btn>
     </div>
@@ -47,6 +48,8 @@
     <p class="texto-rosa">contacto@additionrh.com</p>
   </v-container>
 </template>
+
+
 <style lang="scss">
 .texto-rosa {
   color: #ea5076;
@@ -59,11 +62,148 @@
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
 </style>
+ -->
 
+<template>
+  <div class="container">
+    <form>
+      <label>Name</label>
+      <input type="text" v-model="name" name="name" placeholder="Your Name" />
+      <label>Email</label>
+      <input
+        type="email"
+        v-model="email"
+        name="email"
+        placeholder="Your Email"
+      />
+      <label>Message</label>
+      <textarea
+        name="message"
+        v-model="message"
+        cols="30"
+        rows="5"
+        placeholder="Message"
+      >
+      </textarea>
+
+      <input type="submit" value="Send" />
+    </form>
+  </div>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  display: block;
+  margin: auto;
+  text-align: center;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+  width: 50%;
+}
+
+label {
+  float: left;
+}
+
+input[type="text"],
+[type="email"],
+textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type="submit"] {
+  background-color: #4caf50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type="submit"]:hover {
+  background-color: #45a049;
+}
+</style>
 
 <script>
+import emailjs from 'emailjs-com';
+export default {
+  name: 'ContactUs',
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: ''
+    }
+  },
+  methods: {
+    sendEmail(e) {
+      try {
+        emailjs.sendForm('service_e1qd3r9', 'template_t3rdqq9', e.target, 'cqG37n5OJJU7ta-J5', {
+          name: this.name,
+          email: this.email,
+          message: this.meessage
+        })
+      } catch (error) {
+          console.log({error})
+      }
+      // Reset form field
+      this.name = ''
+      this.email = ''
+      this.message = ''
+    },
+  }
+}
+</script>
+
+<!-- <script>
+import emailjs from "emailjs-com";
 export default {
   data: () => ({
+    name: "ContactUs",
+    data() {
+      return {
+        nombre: "",
+        email: "",
+        mensaje: "",
+      };
+    },
+    methods: {
+      sendEmail(e) {
+        try {
+          emailjs.sendForm(
+            "service_e1qd3r9",
+            "template_t3rdqq9",
+            e.target,
+            "cqG37n5OJJU7ta-J5",
+            {
+              nombre: this.nombre,
+              email: this.email,
+              mensaje: this.mensaje,
+            }
+          );
+        } catch (error) {
+          console.log({ error });
+        }
+        // Reset form field
+        this.name = "";
+        this.email = "";
+        this.message = "";
+      }, -->
+<!--    }/* ,
+
     valid: false,
     firstname: "",
     nameRules: [(v) => !!v || "El nombre es requerido"],
@@ -76,7 +216,7 @@ export default {
     emailRules: [
       (v) => !!v || "E-mail is required",
       (v) => /.+@.+/.test(v) || "Introduce un e-mail válido",
-    ],
+    ], */
   }),
 };
-</script>
+</script> -->
